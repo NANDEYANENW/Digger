7868ddd6bb1be91cf45e57b83e49602677156f5a
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-03 13:55:16 +0900

    Scoreboard周辺を修正。

--- src/main/java/org/hotal/digger/Digger.java
+++ src/main/java/org/hotal/digger/Digger.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -42,61 +43,55 @@ public class Digger extends JavaPlugin implements Listener {
 
        //スコアボードの初期化
         scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-        Objective objective = scoreboard.registerNewObjective("トップ10","トップ10","あなたの順位");
-        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+                Objective objective = scoreboard.registerNewObjective("トップ10","dummy","あなたの順位");
+                objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+            }
 
-    }
-
-    @EventHandler
+        @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         UUID playerID = event.getPlayer().getUniqueId();
         blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
 
         if (Math.random() < 0.02) {
             economy.depositPlayer(event.getPlayer(), 50);
-            event.getPlayer().sendMessage("§350NANNDEを手に入れました");
+            event.getPlayer().sendMessage("50NANNDEを手に入れました");
         }
-
         updateScoreboard(playerID); // playerID を引数として渡す
     }
-
-
-
-
     private void updateScoreboard(UUID playerUUID) {
         Objective objective = scoreboard.getObjective("トップ10");
-
-        // すでに存在するエントリーをクリア
-        for (String entry : scoreboard.getEntries()) {
-            scoreboard.resetScores(entry);
-        }
 
         // ブロックのカウントを降順にソート
         List<Map.Entry<UUID, Integer>> sortedList = blockCount.entrySet().stream()
                 .sorted(Map.Entry.<UUID, Integer>comparingByValue().reversed())
+                .limit(11)  // トップ10 + 現在のプレイヤー
                 .collect(Collectors.toList());
-
-            // プレイヤー自身のランキングを取得
-         int playerRank = 1;
-        for (Map.Entry<UUID, Integer> entry : sortedList) {
-            if (entry.getKey().equals(playerUUID)) {
-                break;
-            }
-            playerRank++;
-        }
-
 
         // トップ10のプレイヤーを表示
         for (int i = 0; i < Math.min(10, sortedList.size()); i++) {
             Map.Entry<UUID, Integer> entry = sortedList.get(i);
             String playerName = Bukkit.getOfflinePlayer(entry.getKey()).getName();
             int score = entry.getValue();
-            objective.getScore(playerName).setScore(score);
+            if (objective.getScore(playerName).getScore() != score) {
+                objective.getScore(playerName).setScore(score);
+            }
         }
 
-        // プレイヤー自身のランキングを表示
-        objective.getScore("あなたの順位: " + playerRank + "位").setScore(0);
+        // プレイヤー自身のランキングとスコアを表示
+        int playerRank = 1;
+        int playerScore = blockCount.get(playerUUID);
+        for (Map.Entry<UUID, Integer> entry : sortedList) {
+            if (entry.getKey().equals(playerUUID)) {
+                break;
+            }
+            playerRank++;
+        }
+        String rankDisplay = "あなたの順位: " + playerRank + "位";
+        if (objective.getScore(rankDisplay).getScore() != playerScore) {
+            objective.getScore(rankDisplay).setScore(playerScore);
+        }
     }
+
 
     private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null ) {
@@ -109,6 +104,4 @@ public class Digger extends JavaPlugin implements Listener {
         economy = rsp.getProvider();
         return economy != null;
     }
-
 }
-
