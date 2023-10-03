6cd7248c6ab4653ffe75308aeaf8e9c01cb7edc7
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-03 15:47:10 +0900

    全体的なコード修正。

--- src/main/java/org/hotal/digger/Digger.java
+++ src/main/java/org/hotal/digger/Digger.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 public class Digger extends JavaPlugin implements Listener {
 
-    private final Map<UUID,Integer> blockCount = new HashMap<>();
+    private final Map<UUID, Integer> blockCount = new HashMap<>();
     private Scoreboard scoreboard;
     private Economy economy;
 
@@ -30,74 +30,27 @@ public class Digger extends JavaPlugin implements Listener {
         if (!setupEconomy()) {
             getLogger().severe("Vaultプラグインが見つかりませんでした。プラグインを無効化します。");
 
-                if (getServer().getPluginManager().getPlugin("Vault") == null) {
-                    getLogger().severe("Vaultプラグインが見つかりません！！");
-                } else {
-                    getLogger().severe("Economyサービスプロバイダが見つかりません！！");
-                }
-                getServer().getPluginManager().disablePlugin(this);
+            if (getServer().getPluginManager().getPlugin("Vault") == null) {
+                getLogger().severe("Vaultプラグインが見つかりません！！");
+            } else {
+                getLogger().severe("Economyサービスプロバイダが見つかりません！！");
+            }
+            getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
-       this.getServer().getPluginManager().registerEvents(this,this);
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    // スコアボードの初期化
-                    scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-                    Objective objective = scoreboard.registerNewObjective("トップ10", "dummy", "あなたの順位");
-                    objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-                }
-            }.runTaskLater(this, 20L); // Run 1 second (20 ticks) after the plugin is enabled
-        }
-
-
-        @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        UUID playerID = event.getPlayer().getUniqueId();
-        blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
-
-        if (Math.random() < 0.02) {
-            economy.depositPlayer(event.getPlayer(), 50);
-            event.getPlayer().sendMessage("50NANNDEを手に入れました");
-        }
-        updateScoreboard(playerID); // playerID を引数として渡す
-    }
-    private void updateScoreboard(UUID playerUUID) {
-        Objective objective = scoreboard.getObjective("トップ10");
-
-        // ブロックのカウントを降順にソート
-        List<Map.Entry<UUID, Integer>> sortedList = blockCount.entrySet().stream()
-                .sorted(Map.Entry.<UUID, Integer>comparingByValue().reversed())
-                .limit(11)  // トップ10 + 現在のプレイヤー
-                .collect(Collectors.toList());
-
-        // トップ10のプレイヤーを表示
-        for (int i = 0; i < Math.min(10, sortedList.size()); i++) {
-            Map.Entry<UUID, Integer> entry = sortedList.get(i);
-            String playerName = Bukkit.getOfflinePlayer(entry.getKey()).getName();
-            int score = entry.getValue();
-            if (objective.getScore(playerName).getScore() != score) {
-                objective.getScore(playerName).setScore(score);
+        this.getServer().getPluginManager().registerEvents(this, this);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // スコアボードの初期化
+                scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+                Objective objective = scoreboard.registerNewObjective("トップ10", "dummy", "あなたの順位");
+                objective.setDisplaySlot(DisplaySlot.SIDEBAR);
             }
-        }
-
-        // プレイヤー自身のランキングとスコアを表示
-        int playerRank = 1;
-        int playerScore = blockCount.get(playerUUID);
-        for (Map.Entry<UUID, Integer> entry : sortedList) {
-            if (entry.getKey().equals(playerUUID)) {
-                break;
-            }
-            playerRank++;
-        }
-        String rankDisplay = "あなたの順位: " + playerRank + "位";
-        if (objective.getScore(rankDisplay).getScore() != playerScore) {
-            objective.getScore(rankDisplay).setScore(playerScore);
-        }
+        }.runTaskLater(this, 20L); // Run 1 second (20 ticks) after the plugin is enabled
     }
 
-        //Economyサービスのロギング
     private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             getLogger().warning("Vaultプラグインが見つかりませんでした。");
@@ -115,4 +68,65 @@ public class Digger extends JavaPlugin implements Listener {
         }
         return true;
     }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        UUID playerID = event.getPlayer().getUniqueId();
+        blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
+
+        getLogger().info("Block broken by " + event.getPlayer().getName()); //デバッグ用
+
+        if (Math.random() < 0.02) {
+            economy.depositPlayer(event.getPlayer(), 50);
+            event.getPlayer().sendMessage("50NANNDEを手に入れました");
+        }
+        updateScoreboard(playerID); // playerID を引数として渡す
+    }
+
+    private void updateScoreboard(UUID playerUUID) {
+        Objective objective = scoreboard.getObjective("トップ10");
+
+        // ブロックのカウントを降順にソート
+        List<Map.Entry<UUID, Integer>> sortedList = blockCount.entrySet().stream()
+                .sorted(Map.Entry.<UUID, Integer>comparingByValue().reversed())
+                .limit(11)  // トップ10 + 現在のプレイヤー
+                .collect(Collectors.toList());
+
+        // トップ10のプレイヤーを表示
+        for (int i = 0; i < Math.min(10, sortedList.size()); i++) {
+            Map.Entry<UUID, Integer> entry = sortedList.get(i);
+            String playerName = Bukkit.getPlayer(entry.getKey()).getName();
+            int score = entry.getValue();
+            objective.getScore(playerName).setScore(score);
+        }
+
+        getLogger().info("Updating scoreboard for " + Bukkit.getPlayer(playerUUID).getName());
+
+// プレイヤー自身のランキングとスコアを表示
+        int playerRank = 1;
+        int playerScore = blockCount.get(playerUUID);
+        for (Map.Entry<UUID, Integer> entry : sortedList) {
+            if (entry.getKey().equals(playerUUID)) {
+                break;
+            }
+            playerRank++;
+        }
+        getLogger().info("Updating scoreboard for " + Bukkit.getPlayer(playerUUID).getName());
+
+// プレイヤー自身のランキングとスコアを表示
+        playerRank = 1;
+        playerScore = blockCount.get(playerUUID);
+        for (Map.Entry<UUID, Integer> entry : sortedList) {
+            if (entry.getKey().equals(playerUUID)) {
+                break;
+            }
+            playerRank++;
+        }
+
+        String rankDisplay = "あなたの順位: " + playerRank + "位";
+        if (objective.getScore(rankDisplay).getScore() != playerScore) {
+            objective.getScore(rankDisplay).setScore(playerScore);
+        }
+    }
 }
+
