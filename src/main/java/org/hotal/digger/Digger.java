d6ad70704b4c05220710689bf804498b1bd96355
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-06 21:34:55 +0900

    途中まで

--- src/main/java/org/hotal/digger/Digger.java
+++ src/main/java/org/hotal/digger/Digger.java
@@ -65,6 +65,25 @@ public class Digger extends JavaPlugin implements Listener {
         startScoreboardUpdater();
     } //起動時の初期化処理ここまで
 
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            getLogger().warning("エラー：Vaultプラグインが見つかりませんでした。");
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            getLogger().warning("エラー:Economyサービスプロバイダが登録されていません");
+            return false;
+        }
+        economy = rsp.getProvider();
+        if (economy == null) {
+            getLogger().warning("エラー：Economyサービスが見つかりません");
+            return false;
+        }
+        return true;
+    }
+
+
     private void startScoreboardUpdater() {
         new BukkitRunnable() {
             @Override
@@ -100,117 +119,86 @@ public class Digger extends JavaPlugin implements Listener {
         }
         return false;
 
-
-    private boolean setupEconomy() { //Vaultのセットアップ、各種エラー
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            getLogger().warning("エラー：Vaultプラグインが見つかりませんでした。");
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            getLogger().warning("エラー:Economyサービスプロバイダが登録されていません");
-            return false;
-        }
-        economy = rsp.getProvider();
-        if (economy == null) {
-            getLogger().warning("エラー：Economyサービスが見つかりません");
-            return false;
-        }
-        return true;
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        // スコアボードやobjectiveがnullかどうかを確認
-        if (scoreboard == null || objective == null) {
-            return; // スコアボードやobjectiveがnullなら処理を終了
-        }
-
-        UUID playerID = event.getPlayer().getUniqueId();
-        blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
-
-
-        if (Math.random() < 0.02) { //2%(1.00で100%)。
-            economy.depositPlayer(event.getPlayer(), 50);
-            event.getPlayer().sendMessage("§a 50NANNDEを手に入れました");
-        }
-        updateScoreboard(playerID); // playerID を引数として渡す
-        event.getPlayer().setScoreboard(scoreboard);
-
-    }
-
-    private void updateScoreboard(UUID playerUUID) {
-        if (scoreboard == null || objective == null) return; // ここにnullチェックを追加
-
-        Objective objective = scoreboard.getObjective("トップ10");
-        // ブロックのカウントを降順にソート
-        List<Map.Entry<UUID, Integer>> sortedList = blockCount.entrySet().stream()
-                .sorted(Map.Entry.<UUID, Integer>comparingByValue().reversed())
-                .limit(11)  // トップ10 + 現在のプレイヤー
-                .collect(Collectors.toList());
-
-        // トップ10のプレイヤーを表示
-        for (int i = 0; i < Math.min(10, sortedList.size()); i++) {
-            Map.Entry<UUID, Integer> entry = sortedList.get(i);
-            String playerName = Bukkit.getPlayer(entry.getKey()).getName();
-            Player player = Bukkit.getPlayer(entry.getKey());
-            if(player == null) continue;
-            playerName = player.getName();
-            int score = entry.getValue();
-            objective.getScore(playerName).setScore(score);
-        }
-
-
-        // プレイヤー自身のランキングとスコアを表示
-        int playerRank = 1;
-        int playerScore = blockCount.getOrDefault(playerUUID, 0);
-        for (Map.Entry<UUID, Integer> entry : sortedList) {
-            if (entry.getKey().equals(playerUUID)) {
-                break;
+        @EventHandler
+        public void onBlockBreak(BlockBreakEvent event){
+            if (scoreboard == null || objective == null) {
+                return;
             }
-            playerRank++;
-        }
-
-
-        if (playerRank > sortedList.size()) {
-
-            return;
-        }
-
-        String rankDisplay = "あなたの順位: " + playerRank + "位";
-        objective.getScore(rankDisplay).setScore(playerScore);
-
-    }
-    @Override
-    public void onDisable () { //終了処理
-        saveData();
-    }
-
-    private void loadData () { //データ保存メソッド（読み込み）
-        dataFile = new File(getDataFolder(), "player-data.yml");
-        if (!dataFile.exists()) {
-            saveResource("player-data.yml", false);
-        }
-        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
-
-        if (dataConfig.contains("blockCount")) {
-            blockCount.clear();
-            for (String uuidString : dataConfig.getConfigurationSection("blockCount").getKeys(false)) {
-                UUID uuid = UUID.fromString(uuidString);
-                int count = dataConfig.getInt("blockCount." + uuidString);
-                blockCount.put(uuid, count);
+            UUID playerID = event.getPlayer().getUniqueId();
+            blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
+            if (Math.random() < 0.02) {
+                economy.depositPlayer(event.getPlayer(), 50);
+                event.getPlayer().sendMessage("§a 50NANNDEを手に入れました");
             }
         }
-    } //読み込みメソッドここまで
 
-    private void saveData(){ //保存メソッドここから
-        for (UUID uuid : blockCount.keySet()) {
-            dataConfig.set("blockCount." + uuid.toString(), blockCount.get(uuid));
+        private void updateScoreboard (UUID playerUUID){
+            if (scoreboard == null || objective == null) return;
+
+            List<Map.Entry<UUID, Integer>> sortedList = blockCount.entrySet().stream()
+                    .sorted(Map.Entry.<UUID, Integer>comparingByValue().reversed())
+                    .limit(11)
+                    .collect(Collectors.toList());
+
+            for (int i = 0; i < Math.min(10, sortedList.size()); i++) {
+                Map.Entry<UUID, Integer> entry = sortedList.get(i);
+                Player player = Bukkit.getPlayer(entry.getKey());
+                if (player == null) continue;
+                String playerName = player.getName();
+                int score = entry.getValue();
+                objective.getScore(playerName).setScore(score);
+            }
+
+            int playerRank = 1;
+            int playerScore = blockCount.getOrDefault(playerUUID, 0);
+            for (Map.Entry<UUID, Integer> entry : sortedList) {
+                if (entry.getKey().equals(playerUUID)) {
+                    break;
+                }
+                playerRank++;
+            }
+
+            if (playerRank > sortedList.size()) {
+                return;
+            }
+
+            String rankDisplay = "あなたの順位: " + playerRank + "位";
+            objective.getScore(rankDisplay).setScore(playerScore);
         }
-        try {
-            dataConfig.save(dataFile);
-        } catch (IOException e) {
-            getLogger().severe("データファイルの保存中にエラーが発生しました。 " + e.getMessage());
+
+        @Override
+        public void onDisable() {
+            saveData();
         }
-    } //データ保存メソッドここまで
+
+
+        private void loadData () {
+            dataFile = new File(getDataFolder(), "player-data.yml");
+            if (!dataFile.exists()) {
+                saveResource("player-data.yml", false);
+            }
+            dataConfig = YamlConfiguration.loadConfiguration(dataFile);
+
+            if (dataConfig.contains("blockCount")) {
+                blockCount.clear();
+                for (String uuidString : dataConfig.getConfigurationSection("blockCount").getKeys(false)) {
+                    UUID uuid = UUID.fromString(uuidString);
+                    int count = dataConfig.getInt("blockCount." + uuidString);
+                    blockCount.put(uuid, count);
+                }
+            }
+        }
+
+
+    }
+        private void saveData() {
+            for (UUID uuid : blockCount.keySet()) {
+                dataConfig.set("blockCount." + uuid.toString(), blockCount.get(uuid));
+            }
+            try {
+                dataConfig.save(dataFile);
+            } catch (IOException e) {
+                getLogger().severe("データファイルの保存中にエラーが発生しました。 " + e.getMessage());
+            }
+        }
 }
