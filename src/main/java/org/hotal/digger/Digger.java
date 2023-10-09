dda629ef6413c12f51d99ea0deb65882e6443d9a
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-09 20:07:10 +0900

    change 50NANNDEの獲得確率の変更

--- src/main/java/org/hotal/digger/Digger.java
+++ src/main/java/org/hotal/digger/Digger.java
@@ -145,7 +145,7 @@ public class Digger extends JavaPlugin implements Listener {
         }
         UUID playerID = event.getPlayer().getUniqueId();
         blockCount.put(playerID, blockCount.getOrDefault(playerID, 0) + 1);
-        if (Math.random() < 0.03) { //3%
+        if (Math.random() < 0.02) { //2%
             economy.depositPlayer(event.getPlayer(), 50); //50NANNDE 追加
             event.getPlayer().sendMessage("§a 50NANNDEを手に入れました。");
         }
